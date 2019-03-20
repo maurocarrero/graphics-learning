1aadb29a6064e0a41fcfcbad8d3706b0fcaa7077
Mauro Carrero <mauro.carrero@sabre.com>
2019-03-20 09:15:59 -0300

    chore(session)
    add rect example 01

--- src/session.js
+++ src/session.js
@@ -1,74 +1,40 @@
-define(['fabric'], function (fabric) {
+define(['fabric', 'utils'], function (fabric, utils) {
   const W = 600;
   const H = 600;
-  const RECT_COLOR = '#191919'; 
-  const CIRCLE_COLOR = '#DE0000'; 
+  const RECT_COLOR = '#007E00';
+  const SQR_SIDE = 200;
 
   return {
     runNative: function () {
-      const canvas = document.getElementById('native');
+      const canvas = utils.setupCanvas(document.getElementById('native'), W, H);
       const ctx = canvas.getContext('2d');
-
-      // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
-      const domRect = canvas.getBoundingClientRect();
-      const W = domRect.width;
-      const H = domRect.height;
-
-      // https://www.html5rocks.com/en/tutorials/canvas/hidpi/
-      const ratio = window.devicePixelRatio;
-
-      canvas.width = W * ratio;
-      canvas.height = H * ratio;
-
-      canvas.style.width = `${W}px`;
-      canvas.style.height = `${H}px`;
-
-      ctx.scale(ratio, ratio);
 
       ctx.fillStyle = RECT_COLOR;
       ctx.fillRect(
-        0,                // x or left
-        0,                // y or top
-        W,                // width
-        H                 // height
+        W / 2,            // x or left
+        H / 2,            // y or top
+        SQR_SIDE,         // width
+        SQR_SIDE          // height
       );
-      
-      ctx.fillStyle = CIRCLE_COLOR;
-      ctx.arc(
-        W / 2,
-        H / 2,
-        W / 4,            // radius
-        0,                // start angle
-        Math.PI * 2       // end angle
-      );     
-      ctx.fill();
     },
 
     runFabric: function () {
       const canvasElement = document.getElementById('fabric');
-      const canvas = new fabric.Canvas(canvasElement);
-
-      canvas.setHeight(W);
-      canvas.setWidth(H);
+      const canvas = new fabric.Canvas(canvasElement, { 
+        backgroundColor: 'lightblue',
+        width: W,
+        height: H
+      });
 
       const rect = new fabric.Rect({
-        left: 0,
-        top: 0,
-        width: canvas.getWidth(),
-        height: canvas.getHeight(),
+        left: canvas.getWidth() / 2,
+        top: canvas.getHeight() / 2,
+        width: SQR_SIDE,
+        height: SQR_SIDE,
         fill: RECT_COLOR
       });
 
-      const circle = new fabric.Circle({
-        left: canvas.getWidth() / 2,
-        top: canvas.getHeight() / 2,
-        radius: canvas.getWidth() / 4,
-        fill: CIRCLE_COLOR,
-        originX: 'center',
-        originY: 'center'
-      });
-
-      canvas.add(rect, circle);
+      canvas.add(rect);
     }
   };
 });
